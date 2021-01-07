e3b569f64af5d121f55b5f00489c756540953e03
wnghks9457 <76986467+wnghks9457@users.noreply.github.com>
2021-01-07 12:43:32 +0900

    initial commit

---
+++ amgn/Assets/GameManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    [SerializeField]
+    private GameObject poop;
+
+    private int score;
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private Text scoreTxt;
+    [SerializeField]
+    private Transform objbox;
+    [SerializeField]
+    private Text bestScore;
+    [SerializeField]
+    private GameObject panel;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool stopTrigger = true;
+    public void GameOver()
+    {
+        stopTrigger = false;
+        StopCoroutine(CreatepoopRoutine());
+
+        if (score >= PlayerPrefs.GetInt("BestScore", 0))
+            PlayerPrefs.SetInt("BestScore", score);
+
+        bestScore.text = PlayerPrefs.GetInt("BestScore", 0).ToString();
+        panel.SetActive(true);
+
+    }
+
+    public void GameStart()
+    {
+        score = 0;
+        scoreTxt.text = "Score : " + score;
+        stopTrigger = true;
+        StartCoroutine(CreatepoopRoutine());
+        panel.SetActive(false);
+    }
+
+    public void Score()
+    {
+        if(stopTrigger)
+        score++;
+        scoreTxt.text = "Store : " + score;
+    }
+
+    IEnumerator CreatepoopRoutine()
+    {
+        while (stopTrigger)
+        {
+            CreatePoop();
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    private void CreatePoop()
+    {
+        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(UnityEngine.Random.Range(0.0f, 1.0f), 1.1f, 0));
+        pos.z = 0.0f;
+        Instantiate(poop, pos, Quaternion.identity);
+    }
+}
